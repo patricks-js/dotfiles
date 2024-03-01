ddcb6823030b41476eb89356eb9dc0967b2917b9
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-03-01 15:19:14 -0300

    first settings in .config

---
+++ .config/fish/config.fish
@@ -0,0 +1,43 @@
+#
+# ███████╗██╗███████╗██╗  ██╗
+# ██╔════╝██║██╔════╝██║  ██║
+# █████╗  ██║███████╗███████║
+# ██╔══╝  ██║╚════██║██╔══██║
+# ██║     ██║███████║██║  ██║
+# ╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝
+# A smart and user-friendly command line
+# https://fishshell.com/
+# cSpell:words shellcode pkgx direnv
+
+set -g fish_greeting
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    starship init fish | source
+    zoxide init fish | source
+end
+
+# List Directory
+alias ls="lsd"
+alias l="ls -l"
+alias la="ls -a"
+alias lla="ls -la"
+alias lt="ls --tree"
+
+# Handy change dir shortcuts
+abbr .. 'cd ..'
+abbr ... 'cd ../..'
+abbr .3 'cd ../../..'
+abbr .4 'cd ../../../..'
+abbr .5 'cd ../../../../..'
+
+# Always mkdir a path (this doesn't inhibit functionality to make a single dir)
+abbr mkdir 'mkdir -p'
+
+# Fixes "Error opening terminal: xterm-kitty" when using the default kitty term to open some programs through ssh
+alias ssh='kitten ssh'
+
+# Vim editor
+set -Ux EDITOR nvim
+
+alias vim=nvim

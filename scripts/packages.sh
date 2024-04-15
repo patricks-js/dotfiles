e252ef9d77925cefb4efb72428c97006b52ed4d2
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-04-15 14:44:55 -0300

    update scripts

---
+++ scripts/packages.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+
+export base=(
+  "bluez"
+  "bluez-utils"
+  "pipewire"
+  "pipewire-jack"
+  "pipewire-alsa"
+  "pipewire-audio"
+  "pipewire-pulse"
+  "wireplumber"
+  "ufw"
+  "sddm"
+  "ufw-extras"
+  "openssh"
+  "mesa"
+  "libva-intel-driver"
+  "intel-media-driver"
+  "vulkan-intel"
+  "ffmpeg"
+  "libjpeg-turbo"
+  "libpng"
+  "libwebp"
+  "imagemagick"
+  "xdg-user-dirs"
+  "man-pages"
+  "man-db"
+  "zip"
+  "unzip"
+)
+
+export cli_toos=(
+  "fish"
+  "wget"
+  "neovim"
+  "zoxide"
+  "tokei"
+  "cava"
+  "neofetch"
+  "tmux"
+  "stow"
+  "bat"
+  "fzf"
+  "starship"
+  "lsd"
+  "gitflow-avh"
+  "gitflow-fishcompletion-avh"
+  "git-crypt"
+  "btop"
+)
+
+export assets=(
+  "noto-fonts"
+  "noto-fonts-cjk"
+  "noto-fonts-emoji"
+  "papirus-icon-theme"
+  "bibata-cursor-theme-bin"
+  "ttf-jetbrains-mono-nerd"
+  "ttf-material-design-icons-extended"
+)
+
+export apps=(
+  "obsidian"
+  "discord"
+  "brave-bin"
+  "visual-studio-code-bin"
+  "mpv"
+  "obs-studio"
+  "firefox"
+  "stremio"
+)
+
+export hyrpand=(
+  "hyprland"
+  "hyprpaper"
+  "xdg-desktop-portal-hyprland" 
+  "waybar" 
+  "grim" 
+  "slurp"
+  "swappy"
+  "cliphist"
+  "hyprlock"
+  "hypridle"
+  "nwg-look"
+)
+
+export gnome=(
+  "gnome"
+  "gnome-tweaks"
+  "gnome-boxes"
+  "pika-backup"
+)

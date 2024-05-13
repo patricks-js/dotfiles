11f52d46b220c73fd051dc0dc59590600cac60b4
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-05-13 15:36:06 -0300

    update configs

---
+++ .config/hypr/scripts/wallpaper.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+# Cache file for holding the current wallpaper
+cache_file="$HOME/.cache/current_wallpaper"
+blurred="$HOME/.cache/blurred_wallpaper.png"
+square="$HOME/.cache/square_wallpaper.png"
+rasi_file="$HOME/.cache/current_wallpaper.rasi"
+
+blur="50x30"
+
+# Create cache file if not exists
+if [ ! -f $cache_file ] ;then
+    touch $cache_file
+    echo "$HOME/wallpaper/default.png" > "$cache_file"
+fi
+
+# Create rasi file if not exists
+if [ ! -f $rasi_file ] ;then
+    touch $rasi_file
+    echo "* { current-image: url(\"$HOME/wallpaper/default.png\", height); }" > "$rasi_file"
+fi
+
+current_wallpaper=$(cat "$cache_file")
+
+case $1 in
+
+    # Load wallpaper from .cache of last session 
+    "init")
+        sleep 1
+        if [ -f $cache_file ]; then
+            wal -q -i $current_wallpaper
+        else
+            wal -q -i ~/wallpaper/
+        fi
+    ;;
+
+    # Select wallpaper with rofi
+    "select")
+        sleep 0.2
+        selected=$( find "$HOME/wallpaper" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) -exec basename {} \; | sort -R | while read rfile
+        do
+            echo -en "$rfile\x00icon\x1f$HOME/wallpaper/${rfile}\n"
+        done | rofi -dmenu -i -replace -config ~/dotfiles/rofi/config-wallpaper.rasi)
+        if [ ! "$selected" ]; then
+            echo "No wallpaper selected"
+            exit
+        fi
+        wal -q -i ~/wallpaper/$selected
+    ;;
+
+    # Randomly select wallpaper 
+    *)
+        wal -q -i ~/wallpaper/
+    ;;
+
+esac
+
+# ----------------------------------------------------- 
+# get wallpaper image name
+# ----------------------------------------------------- 
+newwall=$(echo $wallpaper | sed "s|$HOME/wallpaper/||g")
+
+# ----------------------------------------------------- 
+# Set the new wallpaper
+# -----------------------------------------------------
+transition_type="wipe"
+# transition_type="outer"
+# transition_type="random"
+
+killall hyprpaper
+wal_tpl=$(cat $HOME/dotfiles/.config/.settings/hyprpaper.tpl)
+output=${wal_tpl//WALLPAPER/$wallpaper}
+echo "$output" > $HOME/dotfiles/.config/hypr/hyprpaper.conf
+hyprpaper &
+
+if [ "$1" == "init" ] ;then
+    echo ":: Init"
+else
+    sleep 1
+    notify-send "Changing wallpaper ..." "with image $newwall"
+    
+    # ----------------------------------------------------- 
+    # Reload Hyprctl.sh
+    # -----------------------------------------------------
+    $HOME/.config/ml4w-hyprland-settings/hyprctl.sh &
+fi
+
+# ----------------------------------------------------- 
+# Created blurred wallpaper
+# -----------------------------------------------------
+if [ "$1" == "init" ] ;then
+    echo ":: Init"
+else
+    notify-send "Creating blurred version ..." "with image $newwall"
+fi
+
+magick $wallpaper -resize 75% $blurred
+echo ":: Resized to 75%"
+if [ ! "$blur" == "0x0" ] ;then
+    magick $blurred -blur $blur $blurred
+    echo ":: Blurred"
+fi
+
+# ----------------------------------------------------- 
+# Write selected wallpaper into .cache files
+# ----------------------------------------------------- 
+echo "$wallpaper" > "$cache_file"
+echo "* { current-image: url(\"$blurred\", height); }" > "$rasi_file"
+
+# ----------------------------------------------------- 
+# Send notification
+# ----------------------------------------------------- 
+
+if [ "$1" == "init" ] ;then
+    echo ":: Init"
+else
+    notify-send "Wallpaper procedure complete!" "with image $newwall"
+fi
+
+echo "DONE!"
